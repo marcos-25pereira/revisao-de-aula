06441aa5a6d858f9ce795ad5cefad6fd1749e9d6
marcos-25pereira <marcos.santos.2@academico.ifpb.edu.br>
2025-07-04 14:47:04 +0000

    atividades

---
+++ .py
@@ -0,0 +1,10 @@
+
+numeros_impares = [i for i in range(1, 20, 2)]
+
+numeros_pares = [i for i in range(0, 20, 2)]
+
+print("20 primeiros números ímpares:")
+print(numeros_impares)
+
+print("\n20 primeiros números pares:")
+print(numeros_pares)
